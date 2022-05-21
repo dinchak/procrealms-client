d3abb4b0a7289aeccc8d90568ae1ed1af24979e2
Tom Dinchak <dinchak@gmail.com>
2022-05-20 23:29:38 -0700

    add login page, help page, preferences

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const Conn = require('./lib/conn')
+const Prefs = require('./lib/prefs')
 const UI = require('./lib/ui')
+
+let prefs = Prefs.read()
 
 exports.init = function (program) {
   UI.init(program)
-  Conn.init(onConnect)
+  Conn.init(onConnect, onClose)
 }
 
 function onConnect () {
-  Conn.send('login', { name: 'Bignub', password: 'asd' })
+  if (prefs) {
+    Conn.send('token', { name: prefs.name, token: prefs.token })
+  } else {
+    UI.showLogin('name')
+  }
+}
+
+function onClose () {
+  Conn.init(onConnect, onClose)
 }
