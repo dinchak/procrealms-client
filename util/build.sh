1f3018437c1d92258966f350f7810c4b1889f92b
Tom Dinchak <dinchak@gmail.com>
2022-05-23 01:51:56 -0700

    fix browser bugs, add uglifyjs build step

--- util/build.sh
+++ util/build.sh
@@ -1,1 +1,1 @@
-browserify ../index.js --ignore-missing -o ../www/js/bundle.js
+browserify ../index.js --ignore-missing | uglifyjs --compress drop_debugger=false --mangle eval=false -b beautify=false,ascii_only=true > ../www/js/bundle.js
