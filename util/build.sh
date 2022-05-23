7724c18ec04cf93ff9cc0bc5d9806c9c92eb40eb
Tom Dinchak <dinchak@gmail.com>
2022-05-23 02:04:38 -0700

    added envify

--- util/build.sh
+++ util/build.sh
@@ -1,1 +1,1 @@
-browserify ../index.js --ignore-missing | uglifyjs --compress drop_debugger=false --mangle eval=false -b beautify=false,ascii_only=true > ../www/js/bundle.js
+browserify ../index.js --ignore-missing -t [ envify --NODE_ENV=production ] | uglifyjs --compress drop_debugger=false --mangle eval=false -b beautify=false,ascii_only=true > ../www/js/bundle.js
